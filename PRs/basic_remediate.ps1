5cbab2a4845731b33cfd1e4b02b5c9f762400682
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2022-09-09 14:22:46 +0100

    Update basic_remediate.ps1

--- PRs/basic_remediate.ps1
+++ PRs/basic_remediate.ps1
@@ -6,10 +6,10 @@ $RegType = "String"
 
 Try {
     New-Item $RegPath -Force | New-ItemProperty -Name $RegName -Value $RegValue -PropertyType $RegType -Force | Out-Null
-    #Exit 0
+    Exit 0
 }
 Catch {
     $errMsg = $_.Exception.Message
     Write-Error $errMsg
-    #Exit 1 
+    Exit 1 
 }
